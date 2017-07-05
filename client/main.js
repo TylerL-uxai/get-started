e813a411b43e62c02e2cf13c7837a230e71cd614
Tyler <tylerl-uxai@gmail.com>
2017-07-05 02:55:02 -0500

    routing added but not configured

--- client/main.js
+++ client/main.js
@@ -2,8 +2,10 @@ import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 // import { name as Main } from '../imports/main/main';
 import Main from '../imports/main/main';
+import uiRouter from 'angular-ui-router';
 
 angular.module('simple-todos', [
   angularMeteor,
-  Main.name
+  Main.name,
+  uiRouter
 ]);
